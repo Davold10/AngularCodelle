c1e3155b615d29d82ecf6c62096f7c7cedaf909e
Davold <dublas3.1998@gmail.com>
2019-10-21 15:11:18 -0500

    Create components
    Created components: NavBar,Home,Services, Us & footer

--- Reforzamiento/src/app/app.module.ts
+++ Reforzamiento/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesComponent } from './components/servicios/services.component';
+import { NosotrosComponent } from './components/Nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    ServicesComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule
---
+++ Reforzamiento/src/app/components/Nosotros/nosotros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-nosotros',
+    templateUrl:'./nosotros.component.html',
+    styleUrls:['./nosotros.component.css']
+})
+
+export class NosotrosComponent{
+
+}
